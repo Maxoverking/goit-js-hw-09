a5335ffebacc5dfb315e835df20d00f2dee80262
Maxoverking <avemax7@gmail.com>
2022-10-22 22:03:04 +0200

    первое задание

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,28 @@
+const startButton = document.querySelector('[data-start]');
+const stopButton = document.querySelector('[data-stop]');
 
+let timerId = null;
+
+startButton.addEventListener('click', startInterval);
+stopButton.addEventListener('click', stopInterval);
+
+function startInterval() {
+    timerId = setInterval(() => {
+        console.log('startInterval');
+        document.body.style.backgroundColor = getRandomHexColor(); 
+        startButton.setAttribute('disabled', true);
+        stopButton.addEventListener('click', stopInterval);
+    }, 1000);
+}
+
+function stopInterval() {
+    console.log('clearInterval');
+    clearInterval(timerId);
+    document.body.style.backgroundColor = '';
+    startButton.removeAttribute('disabled');
+    stopButton.removeEventListener('click', stopInterval);
+}
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
